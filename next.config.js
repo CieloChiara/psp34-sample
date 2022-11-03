2956e3ab7c0b50b43dc0c7f0a4a803bc601bbb2e
CieloChiara <cielo.chiara701@gmail.com>
2022-11-03 20:41:32 +0900

    Set sub dirctory

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+const SUB_DIRECTORY = "/psp34_shiden/ui/viewer";
+
+module.exports = nextConfig
